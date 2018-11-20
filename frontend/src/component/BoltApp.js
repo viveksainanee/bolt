afcc29db636404e1d990869807d120a9c537dcaf
Vivek Sainanee <viveksainanee@gmail.com>
2018-11-20 13:14:49 -0800

    added react components for v1

--- frontend/src/component/BoltApp.js
+++ frontend/src/component/BoltApp.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Dashboard from './Dashboard';
 import Portal from './Portal';
+import './BoltApp.css';
 
 class BoltApp extends Component {
   render() {
-    return <div>{this.props.currUser ? <Dashboard /> : <Portal />}</div>;
+    // Temporarily hardcoding to only show dashboard
+    return (
+      <div className="BoltApp">
+        <Dashboard />
+      </div>
+    );
+    // return <div>{this.props.currUser ? <Dashboard /> : <Portal />}</div>;
   }
 }
 
---
+++ frontend/src/component/TaskColumn.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import AddTaskForm from './AddTaskForm';
+import './TaskColumn.css';
+
+class TaskColumn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //TODO: connect to redux
+      tasks: []
+    };
+    this.addTaskToColumn = this.addTaskToColumn.bind(this);
+  }
+
+  addTaskToColumn(task) {
+    console.log(task);
+
+    // this.setState({
+    //   tasks: [...this.state.tasks, task]
+    // });
+  }
+
+  render() {
+    let tasks = this.state.tasks;
+    return (
+      <div className="TaskColumn">
+        <h2> {this.props.type}</h2>
+        <div>
+          {tasks}
+          <AddTaskForm addTaskToColumn={this.addTaskToColumn} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TaskColumn;
